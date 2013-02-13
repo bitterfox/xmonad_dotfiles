8c67c29f80a3de676a16d5c15cfc707d36c126d9
bitter_fox <devnull@localhost>
2013-02-13 20:48:15 +0900

    Add comment: .xmonad/xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -21,12 +21,12 @@ import qualified Data.Map as M
 tall = Tall 1 (3/100) (1/2)
 
 main = do
-    spawn "killall trayer ; trayer --edge top --align right --SetDockType true --SetPartialStrut false --expand true --width 10 --widthtype percent --transparent false --tint 0x000000 --height 20" -- gnome-sound-appletのアイコンが黒一色でない場合は--transparent trueにすると統一感があっていいです。
+    spawn "killall trayer ; trayer --edge top --align right --SetDockType true --SetPartialStrut false --expand true --width 10 --widthtype percent --transparent false --tint 0x000000 --height 20" -- gnome-sound-appletのアイコンが黒一色でない場合は--transparent trueにすると統一感があっていいです。 -- GNOMEのトレイを起動
 --    spawn "gnome-power-manager"
-    spawn "killall nm-applet ; nm-applet"
-    spawn "gnome-sound-applet" -- gnome-volume-control-applet?
-    spawn "nautilus --no-default-window --no-desktop"
-    spawn "dropbox start"
+    spawn "killall nm-applet ; nm-applet" -- ネット接続のアプレットを起動
+    spawn "gnome-sound-applet" -- gnome-volume-control-applet? -- ボリューム変更のアプレットを起動
+    spawn "nautilus --no-default-window --no-desktop" -- nautilusを起動(なんのために起動するのかわからない)
+    spawn "dropbox start" -- dropboxを起動させて同期できるようにする
     xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
@@ -39,9 +39,10 @@ main = do
         , borderWidth = 3
         , normalBorderColor  = "#5382a1" -- Java blue
         , focusedBorderColor = "#e76f00" -- Java orange
-        , focusFollowsMouse = False
+        , focusFollowsMouse = False -- マウスの移動でフォーカスが映らないように
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_l), spawn "gnome-screensaver-command --lock") -- Lock
+        [
+          ((mod4Mask .|. shiftMask, xK_l), spawn "gnome-screensaver-command --lock") -- Lock
         , ((mod4Mask .|. shiftMask, xK_s), spawn "gnome-screensaver-command --lock ; dbus-send --print-reply --system --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend") -- Lock & Suspend
         , ((controlMask, xK_Print), spawn "gnome-screenshot -c")
         , ((0, xK_Print), spawn "gnome-screenshot")
@@ -75,7 +76,9 @@ main = do
         , ((mod4Mask .|. controlMask .|. shiftMask, xK_Down), shiftToNext >> nextWS)
         , ((mod4Mask .|. controlMask .|. shiftMask, xK_Right), shiftToNext >> nextWS)
         ]`additionalKeysP`
-        [ ("<XF86AudioLowerVolume>", setMute(False) >> lowerVolume 3 >> return ())
+        [
+        -- ボリューム周り
+          ("<XF86AudioLowerVolume>", setMute(False) >> lowerVolume 3 >> return ())
         , ("<XF86AudioRaiseVolume>", setMute(False) >> raiseVolume 3 >> return ())
         , ("<XF86AudioMute>",        setMute(False) >> setVolume 0   >> return ()) -- toggleMuteで問題がなければそうすると良いです。
         ]
