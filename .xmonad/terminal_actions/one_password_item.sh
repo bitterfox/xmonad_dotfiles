bd800c02d0697b55af9dbd9d466b506608c8483c
yoshida.shinya <yoshida.shinya@linecorp.com>
2022-03-18 22:25:28 +0900

    Improve one password

--- .xmonad/terminal_actions/one_password_item.sh
+++ .xmonad/terminal_actions/one_password_item.sh
@@ -8,14 +8,17 @@ output=$2
 #item=`op --cache get item $item_uuid`
 item=`$basedir/one_password_get_item.sh $item_uuid`
 
-username=`jq -r '.details.fields[] | select(.name == "username") | .value' <<< $item`
+#fields=`jq '.details.fields + [.details.sections[].fields[]]' <<< $item`
+fields=`jq '.details.fields' <<< $item`
 
 if [ $? -ne 0 ]; then
 item=`$basedir/one_password_get_item.sh --evict-cache $item_uuid`
-username=`jq -r '.details.fields[] | select(.name == "username") | .value' <<< $item`
+#fields=`jq '.details.fields + [.details.sections[].fields[]]' <<< $item`
+fields=`jq '.details.fields' <<< $item`
 fi
 
-password=`jq -r '.details.fields[] | select(.name == "password") | .value' <<< $item`
+username=`jq -r '.[] | select(.designation == "username") | .value' <<< $fields`
+password=`jq -r '.[] | select(.designation == "password") | .value' <<< $fields`
 
 title=`jq -r '.overview.title' <<< $item`
 url=`jq -r '.overview.url' <<< $item`
@@ -25,13 +28,54 @@ if [ -n "$tags" ]; then
     tags="[$tags] "
 fi
 
+show_fields() {
+    fill_username
+    fill_password
+    fill_section
+    copy_username
+    copy_password
+#    copy_other
+}
+
+fill_username() {
+    if [ -n "$username" ]; then
+        echo "Fill username: $username"
+    fi
+}
+fill_password() {
+    if [ -n "$password" ]; then
+        echo "Fill password: ***"
+    fi
+}
+
+fill_section() {
+    jq -c '.details.sections[].fields[]' <<< $item | while read field; do
+        n=`jq -r '.n' <<< $field`
+        k=`jq -r '.k' <<< $field`
+        label=`jq -r '.t' <<< $field`
+        value=`jq -r '.v' <<< $field`
+        if [ "$k" == "concealed" ]; then
+            concealed_value=`jq -r '(.v | length)*"*"' <<< $field`
+            echo "Fill $label($n): $concealed_value"
+        else
+            echo "Fill $label($n): $value"
+        fi
+    done
+}
+
+copy_username() {
+    if [ -n "$username" ]; then
+        echo "Copy username: $username"
+    fi
+}
+copy_password() {
+    if [ -n "$password" ]; then
+        echo "Copy password"
+    fi
+}
+
 header="$tags$title $url"
-result=`cat <<EOF | fzf --preview "echo '$password'" --bind 'alt-p:toggle-preview' --preview-window=down:hidden --header "$header" --bind 'ctrl-r:reload($basedir/one_password_get_item.sh $item_uuid)'
-Fill username: $username
-Fill password
-Copy username: $username
-Copy password
-EOF`
+result=`show_fields | fzf --preview "echo '$password'" --bind 'alt-p:toggle-preview' --preview-window=down:hidden --header "$header" --bind "ctrl-r:reload($basedir/one_password_get_item.sh --evict-cache $item_uuid)"`
 
 if [ $? -ne 0 ]; then
     exit 1
@@ -39,7 +83,7 @@ fi
 
 case "$result" in
     "Fill username: $username" ) echo -n "$username" > "$output" ;;
-    "Fill password" ) echo -n "$password" > "$output" ;;
+    "Fill password: ***" ) echo -n "$password" > "$output" ;;
     "Copy username: $username" ) echo -n "$username" | xsel -b -i ;;
     "Copy password" ) echo -n "$password" | xsel -b -i ;;
 esac
