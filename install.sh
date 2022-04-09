05509ab2e164556329228353d7e2d26451fba94d
yoshida.shinya <yoshida.shinya@linecorp.com>
2022-04-09 19:30:15 +0900

    Fix

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ shift
 PACKAGER_COMMAND="$@"
 if [ -z "PACKAGER_COMMAND" ]; then
     PACKAGER_COMMAND="install"
+fi
 
 PACKAGES="xmonad xmobar ghc libghc-parsec3-dev libghc-split-dev libghc-clock-dev dmenu gmrun trayer gnome-control-center gnome-settings-daemon network-manager-gnome libghc-xmonad-dev libghc-xmonad-contrib-dev ginn gnome-screensaver ttf-ancient-fonts-symbola dunst xdotool"
 
