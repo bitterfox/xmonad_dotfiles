2e5024a80e9fb34549debc95d3ddde95bfa3eb5d
yoshida.shinya <yoshida.shinya@linecorp.com>
2023-04-15 18:49:13 +0900

    Refactor float aware focus

--- .xmonad/lib/XMonad/Util/MyUtils.hs
+++ .xmonad/lib/XMonad/Util/MyUtils.hs
@@ -1,9 +1,22 @@
 
 module XMonad.Util.MyUtils (
-  ifX
+  ifX,
+  caseMaybeJust,
+  andThen
 ) where
 
 import XMonad
 
 ifX :: Bool -> X() -> X()
 ifX cond whenTrue = if cond then whenTrue else return ()
+
+caseMaybeJust :: Maybe a -> (a -> X ()) -> X ()
+caseMaybeJust m f =
+  case m of
+    Just a -> f a
+    Nothing -> return ()
+
+andThen cmp1 cmp2 a b = do
+  let c = cmp1 a b
+  if c == EQ then cmp2 a b
+  else c
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -109,6 +109,8 @@ import XMonad.Util.VirtualMouse
 import XMonad.Util.DocksSupport
 import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, ToggleStruts(..))
 
+import XMonad.Actions.FloatAwareFocus
+
 black = "#4E4B42"
 brightBlack = "#635F54"
 gray = "#B4AF9A"
@@ -398,10 +400,7 @@ windowKeys = L.concat $ [
   ]
 
 floatWindowKeys = [
-    ((mod4Mask, xK_t), do
-                         withWindowSet $ \s -> spawn $ "echo '" ++ (show $ sortedFloats' s) ++ "' >> /tmp/xmonad.debug.floats"
-                         windows floatFocusDown
-                         withWindowSet $ \s -> spawn $ "echo '" ++ (show $ W.integrate' $ W.stack $ W.workspace $ W.current s) ++ "' >> /tmp/xmonad.debug.floats")
+    ((mod4Mask, xK_t), windows floatFocusDown)
   , ((mod4Mask .|. shiftMask, xK_t), windows floatFocusUp)
   , ((mod4Mask .|. controlMask, xK_t), withFocused $ windows . W.sink) -- %! Push window back into tiling
   ]
@@ -706,12 +705,6 @@ main = do
 xnot :: X Bool -> X Bool
 xnot x = not <$> x
 
-caseMaybeJust :: Maybe a -> (a -> X ()) -> X ()
-caseMaybeJust m f =
-  case m of
-    Just a -> f a
-    Nothing -> return ()
-
 doForJust f m = caseMaybeJust m f
 
 isDialog = ask >>= \w -> liftX $ do
@@ -767,151 +760,6 @@ myNamedScratchpadActionMaybe mns =
 ------------------------------------------------------------------------------------------
 -- Scratchpad
 ------------------------------------------------------------------------------------------
-
-------------------------------------------------------------------------------------------
--- Float aware focus
-------------------------------------------------------------------------------------------
-
-floatAvoidFocusUp, floatAvoidFocusDown :: Ord a => W.StackSet i l a s sd -> W.StackSet i l a s sd
-floatAvoidFocusUp stackSet = W.modify' (floatAvoidFocusUp' stackSet) stackSet
-floatAvoidFocusDown stackSet = W.modify' (floatAvoidFocusDown' stackSet) stackSet
-
-floatAvoidFocusUp', floatAvoidFocusDown' :: Ord a => W.StackSet i l a s sd -> W.Stack a -> W.Stack a
-floatAvoidFocusUp' stackSet stack@(W.Stack t (l:ls) rs) =
-    if M.member l $ W.floating stackSet then
-        let W.Stack t' ls' rs' = floatAvoidFocusUp' stackSet (W.Stack t ls rs) in
-          W.Stack t' (l:ls') rs'
-    else W.Stack l ls (t:rs)
-floatAvoidFocusUp' stackSet stack@(W.Stack t [] rs) = do
-    let (x:xs) = reverse rs
-    if M.member x $ W.floating stackSet then do
-      let W.Stack t' ls' rs' = floatAvoidFocusUp' stackSet (W.Stack t [] $ reverse xs)
-      W.Stack t' (x:ls') rs'
-    else
-      W.Stack x (xs ++ [t]) []
-
---floatAvoidFocusUp' stackSet (W.Stack t ls rs) = do
---  let (lf, ls') = L.partition (isFloat stackSet) ls
---  let (rf, rs') = L.partition (isFloat stackSet) rs
---  let W.Stack t' nls nrs = W.focusUp' $ W.Stack t ls' rs'
---  W.Stack t' (lf ++ nls) (rf ++ nrs)
-floatAvoidFocusUp' stackSet stack@(W.Stack t [] []) = stack
-
-floatAvoidFocusDown' stackSet = reverseStack . (floatAvoidFocusUp' stackSet) . reverseStack
-
-floatAvoidSwapUp stackSet = W.modify' (floatAvoidSwapUp' stackSet) stackSet
-floatAvoidSwapUp' stackSet stack@(W.Stack t ls rs) =
-  if isFloat stackSet t then stack
-  else do
-    let (lf, ls') = span (isFloat stackSet) ls
-    let W.Stack t' nls nrs = swapUp' $ W.Stack t ls' rs
-    W.Stack t' nls ((reverse lf) ++ nrs)
-
-swapUp' :: W.Stack a -> W.Stack a
-swapUp'  (W.Stack t (l:ls) rs) = W.Stack t ls (l:rs)
-swapUp'  (W.Stack t []     rs) = W.Stack t (reverse rs) []
-
-floatFocusUp, floatFocusDown :: Ord a => W.StackSet i l a s sd -> W.StackSet i l a s sd
-floatFocusUp stackSet = W.modify' (floatFocusUp' stackSet) stackSet
-floatFocusDown stackSet = W.modify' (floatFocusDown' stackSet) stackSet
-
-floatFocusUp', floatFocusDown' :: Ord a => W.StackSet i l a s sd -> W.Stack a -> W.Stack a
-floatFocusUp' = floatFocusNext . reverse . sortedFloats
-
-floatFocusDown' = floatFocusNext . sortedFloats
-
-sortedFloats stackSet = L.map fst $ sortedFloats' stackSet
-sortedFloats' stackSet =
-    L.sortBy comparator floats
-        where floats = M.assocs $ W.floating stackSet
-              comparingX = comparing $ \(wid, W.RationalRect x y w h) -> x
-              comparingY = comparing $ \(wid, W.RationalRect x y w h) -> y
-              comparingW = comparing $ \(wid, W.RationalRect x y w h) -> w
-              comparingH = comparing $ \(wid, W.RationalRect x y w h) -> h
-              comparingWid = comparing $ \(wid, W.RationalRect x y w h) -> wid
-              comparator = comparingY `andThen` comparingX `andThen` comparingH `andThen` comparingW `andThen` comparingWid
-
-floatFocusNext floats stack@(W.Stack t ls rs) = do
-    let ws = W.integrate stack
-    let fs = L.reverse $ L.filter (\w -> L.elem w ws) $ floats
-    if fs == [] || fs == [t] then stack
-    else do
-      let fs' = takeWhile (/= t) fs
-      let w = if fs' == [] then last fs else last fs'
-      W.Stack w (L.delete w ls) $ t:(L.delete w rs)
-
-andThen cmp1 cmp2 a b = do
-  let c = cmp1 a b
-  if c == EQ then cmp2 a b
-  else c
-
-floatOnUp = withWindowSet(\s -> do
---  before <- gets windowset
---  caseMaybeJust (W.stack $ W.workspace $ W.current before) $
---    \(W.Stack t ls rs) -> spawn $ "echo 'Current: " ++ (show t) ++ ", " ++ (show ls) ++ ", " ++ (show rs) ++ "' >> /tmp/xmonad.debug.floating"
-  caseMaybeJust (W.stack $ W.workspace $ W.current s) $
-    \(W.Stack t ls rs) -> do
-      if isFloat s t then
-          if (L.filter (isFloat s) ls) == [] then return ()
-          else focusedFloatOnUp
-      else do
-        let (rf, rs') = L.partition (isFloat s) rs
-        let (lf, ls') = L.partition (isFloat s) $ L.dropWhile (isFloat s) ls
-        if (rf ++ lf) == [] then return ()
-        else floatOnUp')
---  before <- gets windowset
---  case W.stack $ W.workspace $ W.current before of
---    Just (W.Stack t ls rs) -> spawn $ "echo 'Current: " ++ (show t) ++ ", " ++ (show ls) ++ ", " ++ (show rs) ++ "' >> /tmp/xmonad.debug.floating"
---    Nothing -> return ())
-
-floatOnUp' = do
---  before <- gets windowset
---  caseMaybeJust (W.stack $ W.workspace $ W.current before) $
---    \(W.Stack t ls rs) -> spawn $ "echo 'Before: " ++ (show t) ++ ", " ++ (show ls) ++ ", " ++ (show rs) ++ "' >> /tmp/xmonad.debug.floating"
-  modifyWindowSet (\s -> W.modify' (\stack@(W.Stack t ls rs) -> do
-    let (rf, rs') = L.partition (isFloat s) $ L.reverse rs
-    let lf' = L.takeWhile (isFloat s) ls
-    let (lf, ls') = L.partition (isFloat s) $ L.dropWhile (isFloat s) ls
-    if (rf ++ lf) == [] then stack
-    else W.Stack t (rf ++ lf' ++ lf ++ ls') $ reverse rs') s)
---  before <- gets windowset
---  case W.stack $ W.workspace $ W.current before of
---    Just (W.Stack t ls rs) -> spawn $ "echo 'After: " ++ (show t) ++ ", " ++ (show ls) ++ ", " ++ (show rs) ++ "' >> /tmp/xmonad.debug.floating"
---    Nothing -> return ()
-
-focusedFloatOnUp = do
---  before <- gets windowset
---  case W.stack $ W.workspace $ W.current before of
---    Just (W.Stack t ls rs) -> spawn $ "echo 'Before: " ++ (show t) ++ ", " ++ (show ls) ++ ", " ++ (show rs) ++ "' >> /tmp/xmonad.debug.floating"
---    Nothing -> return ()
-  windows (\s -> W.modify' (\stack@(W.Stack t ls rs) -> do
-    let (lf, ls') = L.partition (isFloat s) ls
-    if lf == [] then stack
---    else W.Stack t ls' $ rs ++ (reverse lf)) s)
-    else W.Stack t ls' $ (reverse lf) ++ rs) s)
---  before <- gets windowset
---  case W.stack $ W.workspace $ W.current before of
---    Just (W.Stack t ls rs) -> spawn $ "echo 'After: " ++ (show t) ++ ", " ++ (show ls) ++ ", " ++ (show rs) ++ "' >> /tmp/xmonad.debug.floating"
---    Nothing -> return ()
-
-isFloat stackSet window = M.member window $ W.floating stackSet
-
--- | reverse a stack: up becomes down and down becomes up.
-reverseStack :: W.Stack a -> W.Stack a
-reverseStack (W.Stack t ls rs) = W.Stack t rs ls
-
-locateFloat managehook = withWindowSet (\s -> do
-  case W.peek s of
-    Just t ->
-      if isFloat s t then do
-        endo <- runQuery managehook t
-        windows $ appEndo endo
-        return True
-      else
-        return False
-    Nothing -> return False
-  )
-
 
 ------------------------------------------------------------------------------------------
 -- WorkspaceFamily
