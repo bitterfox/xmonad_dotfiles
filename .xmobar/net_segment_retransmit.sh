a29080c6198f0a1a0ede1260cb64a4c6e8639182
yoshida.shinya <yoshida.shinya@linecorp.com>
2023-07-29 19:22:15 +0900

    Optimize net_segment_retransmit script

--- .xmobar/net_segment_retransmit.sh
+++ .xmobar/net_segment_retransmit.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-tcp_columns=`cat /proc/net/snmp | grep "Tcp:" | head -n 1 | sed -r "s/Tcp: (.*)/\1/"`
-tcp_values=`cat /proc/net/snmp | grep "Tcp:" | head -n 2 | tail -n 1 | sed -r "s/Tcp: (.*)/\1/"`
+tcp_values=`cat /proc/net/snmp | grep "Tcp:" | head -n 2 | tail -n 1`
+set -- $tcp_values
 
-#echo $tcp_columns
-#echo $tcp_values
+tcp_columns=`cat /proc/net/snmp | grep "Tcp:" | head -n 1`
 
-while [ -n "$tcp_columns" ]; do
-    if [ "`echo $tcp_columns | sed -r "s/([^ ]+)(.*)/\1/"`" == "RetransSegs" ]; then
-        echo $tcp_values | sed -r "s/([^ ]+)(.*)/\1/"
+for column in $tcp_columns; do
+    if [ "$column" == "RetransSegs" ]; then
+        echo $1
+        break
     fi
-    tcp_columns=`echo $tcp_columns | sed -r "s/([^ ]+)(.*)/\2/"`
-    tcp_values=`echo $tcp_values | sed -r "s/([^ ]+)(.*)/\2/"`
+    shift
 done
