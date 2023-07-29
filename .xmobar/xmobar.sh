82c8cd51d2473e232c657979878cc21126a95c88
yoshida.shinya <yoshida.shinya@linecorp.com>
2023-07-29 21:36:30 +0900

    Reorganize

--- .xmobar/xmobar.sh
+++ .xmobar/xmobar.sh
@@ -5,23 +5,21 @@ basedir=$(dirname $0)
 
 start=`date +%s%N`
 
-battery=`battery`
-cpu_util=`cpu_util`
-cpu_freq=`cpu_freq`
-net=`net_bps`
 wip_task=`wip_task`
-
-cpu_freq_limit=`cpu_freq_limit`
+battery=`battery`
 cpu_temp=`cpu_temp`
 fan_speed=`fan_speed`
+cpu_util=`cpu_util`
+cpu_freq=`cpu_freq`
+cpu_freq_limit=`cpu_freq_limit`
 mem=`memory Mem 🍫`
 swap=`memory Mem 🔃`
+net=`net_bps`
 net_segment_retransmit=`net_segment_retransmit`
 brightness=`brightness`
 volume=`volume`
 jadate=`date "+%_m/%_d(%a) %H:%M"`
 
 end=`date +%s%N`
-
 
 echo "$wip_task | $battery | $cpu_temp$fan_speed | $cpu_util $cpu_freq($cpu_freq_limit) | $mem $swap | $net ($net_segment_retransmit) | ☀$brightness | $volume | $jadate (`printf "%4d" $(((end-start)/1000/1000))` ms)"
