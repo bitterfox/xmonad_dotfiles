c4acfeb97322e87479fd184c5c2a9907da417283
yoshida.shinya <yoshida.shinya@linecorp.com>
2024-11-16 17:50:32 +0900

    Add manage hook and fix avoid StrutsFloat

--- .xmonad/lib/XMonad/Util/ManageHookUtils.hs
+++ .xmonad/lib/XMonad/Util/ManageHookUtils.hs
@@ -1,10 +1,11 @@
 module XMonad.Util.ManageHookUtils (
   avoidStrutsFloat,
-  onCenter, onCenter', onCenter'',
+  onCenter, onCenter', onCenter'', onCenter''',
   onTop, onTop', onTop'',
   onBottom, onBottom', onBottom'',
   onLeft, onLeft', onLeft'',
-  onRight, onRight', onRight''
+  onRight, onRight', onRight'',
+  minWidth
 ) where
 
 import Data.Ratio
@@ -21,6 +22,7 @@ import XMonad.Util.Types
 onCenter = onCenter' 0
 onCenter' spaceRatio = onCenter'' spaceRatio spaceRatio
 onCenter'' spaceRatioV spaceRatioH = (avoidStrutsFloat $ W.RationalRect spaceRatioV (spaceRatioH) (1-spaceRatioV*2) (1-spaceRatioH*2))
+onCenter''' spaceRatioV spaceRatioH f = (avoidStrutsFloat' (W.RationalRect spaceRatioV (spaceRatioH) (1-spaceRatioV*2) (1-spaceRatioH*2)) f)
 
 onTop = onTop' 0
 onTop' spaceRatio = onTop'' spaceRatio spaceRatio
@@ -38,25 +40,33 @@ onRight = onRight' 0
 onRight' spaceRatio = onRight'' spaceRatio spaceRatio
 onRight'' spaceRatioV spaceRatioH = (avoidStrutsFloat $ W.RationalRect (0.5+spaceRatioV) (spaceRatioH) (0.5-spaceRatioV*2) (1-spaceRatioH*2))
 
-avoidStrutsFloat (W.RationalRect x y w h) = do
+avoidStrutsFloat r = avoidStrutsFloat' r $ \r' -> r'
+
+avoidStrutsFloat' (W.RationalRect x y w h) f = do
   win <- ask
   (sid, r) <- liftX $ floatLocation win
   rect <- liftX $ withWindowSet $ \ws -> do
-    case L.find ((sid ==) . W.screen) $ W.screens ws of
-        Just sc -> do
-          let r = screenRect $ W.screenDetail sc
-          let Rectangle rx ry rw rh = r
-          srect <- fmap ($ r) (calcGap $ S.fromList [U,D,L,R])
-          let Rectangle sx sy sw sh = srect
-          let nx = (fromIntegral sx) + (x * (fromIntegral sw))
-          let ny = (fromIntegral sy) + (y * (fromIntegral sh))
-          let nw = (w * (fromIntegral sw))
-          let nh = (h * (fromIntegral sh))
-          return $ (W.RationalRect
-                                ((nx - (fromIntegral rx)) / (fromIntegral rw))
-                                ((ny - (fromIntegral ry)) / (fromIntegral rh))
-                                (nw / (fromIntegral rw))
-                                (nh / (fromIntegral rh))
-                   )
-        Nothing -> return $ (W.RationalRect x y w h)
+    let sc = W.current ws
+    let r = screenRect $ W.screenDetail sc
+    let Rectangle rx ry rw rh = r
+    srect <- fmap ($ r) (calcGap $ S.fromList [U,D,L,R])
+    let Rectangle sx sy sw sh = srect
+    let nx = (fromIntegral sx) + (x * (fromIntegral sw))
+    let ny = (fromIntegral sy) + (y * (fromIntegral sh))
+    let nw = (w * (fromIntegral sw))
+    let nh = (h * (fromIntegral sh))
+    let W.RationalRect nx' ny' nw' nh' = f $ W.RationalRect nx ny nw nh
+    return $ (W.RationalRect
+                   ((nx' - (fromIntegral rx)) / (fromIntegral rw))
+                   ((ny' - (fromIntegral ry)) / (fromIntegral rh))
+                   (nw' / (fromIntegral rw))
+                   (nh' / (fromIntegral rh))
+             )
   customFloating rect
+
+minWidth mw r@(W.RationalRect x y w h) =
+   if w < mw then
+      (W.RationalRect (x-(mw-w)/2) y mw h)
+  else
+      r
+
