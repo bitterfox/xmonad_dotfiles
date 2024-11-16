0103e55baaf9ac4c33b8042448e7cf57db1065e2
yoshida.shinya <yoshida.shinya@linecorp.com>
2024-11-16 17:56:00 +0900

    Fixed kbd repeat config

--- .xmonad/system_scripts/bright/workaround_monitor_bright.sh
+++ .xmonad/system_scripts/bright/workaround_monitor_bright.sh
@@ -3,6 +3,8 @@ export DISPLAY=:0
 
 BITTER_FOX_WMB_SLEEP_ON="${BITTER_FOX_WMB_SLEEP_ON:-3}"
 BITTER_FOX_WMB_SLEEP_OFF="${BITTER_FOX_WMB_SLEEP_ON:-1}"
+BITTER_FOX_WMB_REPEAT_DELAY="${BITTER_FOX_WMB_REPEAT_DELAY:-210}"
+BITTER_FOX_WMB_REPEAT_RATE="${BITTER_FOX_WMB_REPEAT_RATE:-71}"
 
 /usr/bin/xhost +SI:localuser:root
 
@@ -16,7 +18,8 @@ while :; do
         echo "Wait monitor off"
     fi
     while :; do
-        stat=`xset q | grep Monitor`
+        xset_q=`xset q`
+        stat=`echo "$xset_q" | grep Monitor`
         if [ -n "$BITTER_FOX_WMB_DEBUG" ]; then
             echo $stat
         fi
@@ -29,10 +32,22 @@ while :; do
         else
             break
         fi
+
+        auto_repeat_conf=`echo "$xset_q" | grep "auto repeat delay"`
+        auto_repeat_delay=`echo "$auto_repeat_conf" | awk '{print $4}'`
+        auto_repeat_rate=`echo "$auto_repeat_conf" | awk '{print $7}'`
+        if [ -n "$BITTER_FOX_WMB_DEBUG" ]; then
+            echo "auto repeat delay $auto_repeat_delay auto repeat rate $auto_repeat_rate"
+        fi
+        if [ "$auto_repeat_delay" -ne $BITTER_FOX_WMB_REPEAT_DELAY ] || [ "$auto_repeat_rate" -ne $BITTER_FOX_WMB_REPEAT_RATE ]; then
+            xset r rate $BITTER_FOX_WMB_REPEAT_DELAY $BITTER_FOX_WMB_REPEAT_RATE
+        fi
     done
 
     # Wait monitor on
-    echo "Wait monitor on"
+    if [ -n "$BITTER_FOX_WMB_DEBUG" ]; then
+        echo "Wait monitor on"
+    fi
     while :; do
         stat=`xset q | grep Monitor`
         echo $stat
@@ -48,6 +63,8 @@ while :; do
     done
 
     # Reset monitor brightness
-    echo "Reset monitor brightness: `$basedir/get_current.sh`"
+    if [ -n "$BITTER_FOX_WMB_DEBUG" ]; then
+        echo "Reset monitor brightness: `$basedir/get_current.sh`"
+    fi
     $basedir/set.sh `$basedir/get_current.sh`
 done
