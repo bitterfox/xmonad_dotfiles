62bef04cea46d419d9b8be5ca47e24f6b0c092f9
yoshida.shinya <yoshida.shinya@linecorp.com>
2024-12-01 02:58:56 +0900

    mbp2019_ubuntu2004 Update installer

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ if [ -z "PACKAGER_COMMAND" ]; then
     PACKAGER_COMMAND="install"
 fi
 
-PACKAGES="xmonad xmobar ghc libghc-parsec3-dev libghc-split-dev libghc-clock-dev dmenu gmrun trayer gnome-control-center gnome-settings-daemon network-manager-gnome libghc-xmonad-dev libghc-xmonad-contrib-dev ginn gnome-screensaver ttf-ancient-fonts-symbola dunst xdotool"
+PACKAGES="xmonad xmobar ghc cabal-install libghc-parsec3-dev libghc-split-dev libghc-clock-dev dmenu gmrun trayer gnome-control-center gnome-settings-daemon network-manager-gnome libghc-xmonad-dev libghc-xmonad-contrib-dev ginn gnome-screensaver ttf-ancient-fonts-symbola dunst xdotool"
 
 $PACKAGER $PACKAGER_COMMAND $PACKAGES
+
+cabal update
+cabal install --lib xmonad xmonad-contrib extensible-exceptions clock X11 mtl split parsec utf8-string
