b46c5a60968930e95f8afac7340bcfaaa7fb8f0a
yoshida.shinya <yoshida.shinya@linecorp.com>
2025-08-02 12:55:21 +0900

    mbp2019_ubuntu2004 Change script name for AI

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -833,7 +833,7 @@ myScratchpads = [
            onCenter
   , terminalScratchpad "fzf_actions" (Just "\"zsh -c '. $HOME/.fzf.zsh; _fzf_actions_then_echo -n | xsel -b -i'\"") $ onCenter'' 0.1 0.2
   , NS "ai"
-            "bash ~/scripts/xmonad_chatai.sh"
+            "bash ~/scripts/xmonad_ai.sh"
             (className =? "xmonad.AI")
             $ onCenter''' 0.25 0.01 $ minWidth 1600
  ]
