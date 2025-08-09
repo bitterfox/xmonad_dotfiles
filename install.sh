0e657cf919b22399c3ccca0490849ab56d5571ac
yoshida.shinya <yoshida.shinya@linecorp.com>
2025-08-09 19:43:27 +0900

    za9c-r58_ubuntu2404 cleanup install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,11 @@ if [ -z "PACKAGER_COMMAND" ]; then
     PACKAGER_COMMAND="install"
 fi
 
-PACKAGES="xmonad xmobar ghc cabal-install libghc-parsec3-dev libghc-split-dev libghc-clock-dev dmenu gmrun trayer gnome-control-center gnome-settings-daemon network-manager-gnome libghc-xmonad-dev libghc-xmonad-contrib-dev ginn gnome-screensaver ttf-ancient-fonts-symbola dunst xdotool"
+XMONAD_PACKAGES="ghc cabal-install xmonad libghc-parsec3-dev libghc-split-dev libghc-clock-dev libghc-xmonad-dev libghc-xmonad-contrib-dev"
+XMONAD_TOOLS_PACKAGES="xmobar dunst dmenu gmrun trayer ginn xdotool"
+GNOME_PACKAGES="gnome-control-center gnome-settings-daemon gnome-screenshot gnome-screensaver network-manager-gnome"
+FONT_PACKAGES="ttf-ancient-fonts-symbola"
+PACKAGES="$XMONAD_PACKAGES $XMONAD_TOOLS_PACKAGES $GNOME_PACKAGES $FONT_PACKAGES"
 
 sudo $PACKAGER $PACKAGER_COMMAND $PACKAGES
 
