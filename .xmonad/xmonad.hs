d22993716f78df5edcdec9756e76a6360e705556
yoshida.shinya <yoshida.shinya@linecorp.com>
2025-10-12 22:39:54 +0900

    za9c-r58_ubuntu2404 Fix path for xmonad_ai.sh

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -842,7 +842,7 @@ myScratchpads = [
            onCenter
   , terminalScratchpad "fzf_actions" (Just "\"zsh -c '. $HOME/.fzf.zsh; _fzf_actions_then_echo -n | xsel -b -i'\"") $ onCenter'' 0.1 0.2
   , NS "ai"
-            "bash ~/scripts/xmonad_ai.sh"
+            "bash ~/.xmonad/xmonad_ai.sh"
             --(className =? "xmonad.AI")
             (appName =? "chatgpt.com")
             $ onCenter''' 0.25 0.01 $ minWidth 1600
