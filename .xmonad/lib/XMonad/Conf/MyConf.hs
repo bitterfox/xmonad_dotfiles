fd1929465871a1fcaeb799166056bbd03c1e2760
yoshida.shinya <yoshida.shinya@linecorp.com>
2025-10-26 15:13:14 +0900

    mbp-m4pro-vmware_ubuntu2404 refactoring

---
+++ .xmonad/lib/XMonad/Conf/MyConf.hs
@@ -0,0 +1,137 @@
+module XMonad.Conf.MyConf where
+
+import System.Directory
+import System.Exit
+import System.IO
+
+import XMonad
+import XMonad.Core
+import qualified XMonad.StackSet as W
+import XMonad.ManageHook
+import XMonad.Util.Run(runProcessWithInputAndWait, seconds)
+
+import XMonad.Util.MyUtils
+
+import XMonad.Actions.DrawShape (RGB(..))
+import XMonad.Util.PhysicalScreen (EDID(..))
+import XMonad.Util.NamedScratchpad2
+import XMonad.Util.MyNamedScratchpad
+import XMonad.Util.ManageHookUtils
+
+------------------------------------------------------------------------------------------
+-- Color
+------------------------------------------------------------------------------------------
+black = "#4E4B42"
+brightBlack = "#635F54"
+gray = "#B4AF9A"
+darkWhite = "#CDC8B0"
+white = "#DAD4BB"
+red = "#CC654C"
+redRGB = RGB { rgb_red = 204, rgb_green = 101, rgb_blue = 76 }
+blue = "#3BA99F"
+blueRGB = RGB { rgb_red = 59, rgb_green = 169, rgb_blue = 159 }
+
+-- #C14BCC
+purpleRGB = RGB { rgb_red = 193, rgb_green = 75, rgb_blue = 204 }
+-- #4B6BCC
+darkBlueRGB = RGB { rgb_red = 75, rgb_green = 107, rgb_blue = 204 }
+-- #A89F3B
+yellowRGB = RGB { rgb_red = 168, rgb_green = 159, rgb_blue = 59 }
+
+------------------------------------------------------------------------------------------
+-- Application
+------------------------------------------------------------------------------------------
+intellijCommand = "~/bin/idea"
+applications = [
+-- ("Vivaldi (Web browser)", "export GDK_DPI_SCALE=1.02; vivaldi"),
+ ("Vivaldi (Web browser)", "vivaldi"),
+ ("Nautilus (File browser)", "nautilus"),
+ ("Emacs (Editor)", "emacs"),
+ -- ("LINE", "wine '/home/jp21734/.wine/drive_c/users/jp21734/Local Settings/Application Data/LINE/bin/LineLauncher.exe'"),
+ ("LINE", "vivaldi-stable --app='chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html#/'"),
+ ("Configuration", "XDG_CURRENT_DESKTOP=GNOME gnome-control-center"),
+ ("LibreOffice", "libreoffice"),
+ ("JetBrains ToolBox", "~/bin/jetbrains-toolbox-1.14.5179/jetbrains-toolbox"),
+ ("IntelliJ Idea", intellijCommand),
+ ("PulseSecure", "/opt/pulsesecure/bin/pulseUI"),
+ ("Steam", "steam"),
+ ("Slack", "slack"),
+ ("Discord", "discord"),
+ ("Tweetdeck", webApplication "https://tweetdeck.twitter.com/"),
+ ("YouTube", webApplication "https://youtube.com/"),
+ ("DAZN", webApplication "https://dazn.com/")]
+webApplication url = "vivaldi-stable --app=" ++ url
+javaHome = "~/.sdkman/candidates/java/current"
+jshellPath = javaHome ++ "/bin/jshell"
+
+------------------------------------------------------------------------------------------
+-- Display
+------------------------------------------------------------------------------------------
+priorityDisplayEDIDs :: [EDID]
+priorityDisplayEDIDs = [
+  "3840x2160+2294+0",
+  "2294x1432+0+0",
+  "1890x3360+6134+0",
+
+  "00ffffffffffff0010ac4da2534e4a30", -- AW3225
+  "00ffffffffffff0010ac49a2534e4a30",
+  "00ffffffffffff0010acb7414c323332", -- U2720Q 9x16
+  "00ffffffffffff0010acb4414c323332",
+  "00ffffffffffff0010acb5414c323332",
+  "00ffffffffffff00061044a000000000", -- Laptop display
+  "00ffffffffffff0010acb3414c333232", -- U2720Q 16x9
+  "00ffffffffffff0010acb5414c333232"]
+
+------------------------------------------------------------------------------------------
+-- Scratchpad
+------------------------------------------------------------------------------------------
+myScratchpads :: [NamedScratchpad]
+myScratchpads = [
+    terminalScratchpad "mainterm" Nothing $ onCenter' 0.01
+  , terminalScratchpad "term1" Nothing onTop
+  , terminalScratchpad "term2" Nothing onBottom
+  , terminalScratchpad "termL" Nothing onLeft
+  , terminalScratchpad "termR" Nothing onRight
+  , terminalScratchpad "jshell1" (Just jshellPath) onTop
+  , terminalScratchpad "jshell2" (Just jshellPath) onBottom
+  , NS "bunnaru"
+           "google-chrome --renderer-process-limit=1 --new-window --app=http://www.dmm.com/netgame/social/-/gadgets/=/app_id=798209/"
+           (appName =? "www.dmm.com__netgame_social_-_gadgets_=_app_id=798209")
+           (customFloating $ W.RationalRect 0 0.4 0.55 0.6)
+  , NS "艦これ"
+           "google-chrome --renderer-process-limit=1 --new-window --app=http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/"
+           (appName =? "www.dmm.com__netgame_social_-_gadgets_=_app_id=854854")
+           (customFloating $ W.RationalRect 0.55 0.4 0.45 0.6)
+  , NS "rhythmbox"
+           "rhythmbox"
+           (className =? "Rhythmbox")
+           onCenter
+  , terminalScratchpad "fzf_actions" (Just "\"zsh -c '. $HOME/.fzf.zsh; _fzf_actions_then_echo -n | xsel -b -i'\"") $ onCenter'' 0.1 0.2
+  , NS "ai"
+            "bash ~/.xmonad/xmonad_ai.sh"
+            --(className =? "xmonad.AI")
+            (appName =? "chatgpt.com")
+            $ onCenter''' 0.25 0.01 $ minWidth 1600
+ ]
+
+systemActions = [
+  ("Reload", myrestart),
+  ("Lock", spawn "gnome-screensaver-command --lock"),
+  ("Suspend", spawn "systemctl suspend"),
+  ("Logout", io (exitWith ExitSuccess)),
+  ("Shutdown", spawn "systemctl poweroff"),
+  ("Reboot", spawn "systemctl reboot"),
+  ("Toggle game mode", togglegamemode)
+  ]
+
+myrestart = withWindowSet $ myrestart' . W.screen . W.current
+myrestart' sid = do
+  if sid == 0 then
+      spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi"
+  else
+      (viewScreen $ sid - 1) >> (myrestart' $ sid - 1)
+
+togglegamemode = do
+  homeDirectory <- liftIO getHomeDirectory
+  runProcessWithInputAndWait "sh" ["-c", "bash '" ++ homeDirectory ++ "/.xmonad/toggle_game.sh' >> /tmp/xmonad.debug"] "" (seconds 1)
+  myrestart
---
+++ .xmonad/lib/XMonad/Conf/MyTerminalAction.hs
@@ -0,0 +1,181 @@
+module XMonad.Conf.MyTerminalAction where
+
+import qualified Data.Text as T
+import qualified Data.List as L
+import Data.Maybe
+
+import Control.Exception.Extensible as E
+
+import XMonad
+import qualified XMonad.StackSet as W
+
+import XMonad.Actions.TerminalAction
+import XMonad.Actions.TerminalAction.GnomeBackend
+
+import XMonad.Util.ManageHookUtils
+import XMonad.Util.HandleEventHooks
+import XMonad.Util.WorkspaceFamily
+import XMonad.Util.MyUtils
+import XMonad.Conf.MyConf
+
+terminalActionManageHook = onCenter'' 0.3 0.2
+
+myTerminal = GnomeTerminal "xmonad.terminal.action"
+selectWindowTerminalActionTemplate =
+  (terminalActionTemplate "select.window" "~/.xmonad/terminal_actions/select_window.sh" $ onCenter'' 0.1 0.2)
+  .| withFirstLine .|| words .|| head .|| read
+selectActionTerminalActionTemplate =
+  (terminalActionTemplate "select.action" "~/.xmonad/terminal_actions/select_action.sh" terminalActionManageHook)
+  .| withFirstLine
+dmenuRunTerminalAction =
+  (terminalActionTemplate "dmenu.run" "~/.xmonad/terminal_actions/dmenu_run.sh" terminalActionManageHook)
+  .| withFirstLine .>> spawn
+openBrowserHistoryTerminalAction =
+  (terminalActionTemplate "open.history" "~/.xmonad/terminal_actions/select_browser_history.sh" $ onCenter'' 0.1 0.2)
+  .| withFirstLine .|| (\s -> "xdg-open '" ++ s ++ "'") .>> spawn
+copyFromClipboardHistoryListTerminalAction =
+  (terminalActionTemplate "copy.from.clipboard.history.list" "~/.xmonad/terminal_actions/select_clipboard.sh" $ terminalActionManageHook) .>| ()
+copyFromClipboardHistoryAgTerminalAction =
+  (terminalActionTemplate "copy.from.clipboard.history.ag" "~/.xmonad/terminal_actions/select_clipboard.sh ag" $ terminalActionManageHook) .>| ()
+copyFromClipboardHistoryListMultiTerminalAction =
+  (terminalActionTemplate "copy.from.clipboard.history.list.multi" "~/.xmonad/terminal_actions/select_clipboard.sh multi" $ terminalActionManageHook) .>| ()
+onePasswordTerminalAction =
+  (terminalActionTemplate "one.password" "~/.xmonad/terminal_actions/one_password.sh" $ onCenter'' 0.1 0.2)
+  .| withFirstLine .|| (\s -> "xdotool type '" ++ (T.unpack $ T.replace (T.pack "'") (T.pack "'\"'\"'") (T.pack s)) ++ "'") .>> spawn
+openDashboardTerminalAction =
+  (terminalActionTemplate "open.dashboard" "" terminalActionManageHook)
+  .| withoutEmpty .|| (\outputs ->
+                           if head outputs == "alt-enter" then
+                               (False, tail outputs)
+                           else
+                               (True, outputs))
+  .|| (\(appMode, urls) -> L.map (\url -> (if appMode then "vivaldi --app='" else "xdg-open '") ++ url ++ "'") urls)
+  .|| L.map spawn
+  .>> L.foldr (>>) (return ())
+
+myTerminalActions = [
+   (terminalActionTemplate "open.intellij" "~/.xmonad/terminal_actions/open_intellij.sh" terminalActionManageHook)
+   .| withFirstLine .|| ((intellijCommand ++ " ") ++) .>> spawn
+  , dmenuRunTerminalAction
+  , openBrowserHistoryTerminalAction
+  , copyFromClipboardHistoryListTerminalAction
+  , copyFromClipboardHistoryAgTerminalAction
+  , copyFromClipboardHistoryListMultiTerminalAction
+  , onePasswordTerminalAction
+  , selectWindowTerminalActionTemplate .>| ()
+  , selectActionTerminalActionTemplate .>| ()
+  , openDashboardTerminalAction]
+
+myTerminalActionHandleEventHook = keepWindowSizeHandleEventHook $ L.foldr (<||>) (return False) $ L.map (terminalQuery myTerminal) myTerminalActions
+
+openIntelliJTerminalAction = do
+  runNamedTerminalAction myTerminal myTerminalActions "open.intellij"
+
+data BrowserHistoryTerminalActionState = BrowserHistoryTerminalActionState Int deriving (Typeable)
+instance ExtensionClass BrowserHistoryTerminalActionState where
+  initialValue = BrowserHistoryTerminalActionState 0
+runOpenBrowserHistoryTerminalAction = do
+  let sort = ["often", "recent"]
+  runCyclicTerminalAction myTerminal "open.browser.history" $
+                          L.map (openBrowserHistoryTerminalAction .<) sort
+
+runCopyFromClipboardHistoryTerminalAction = do
+  runCyclicTerminalAction myTerminal "copy.from.clipboard.history" $
+                          [copyFromClipboardHistoryListTerminalAction, copyFromClipboardHistoryAgTerminalAction, copyFromClipboardHistoryListMultiTerminalAction]
+
+runOnePasswordTerminalAction = do
+  runNamedTerminalAction myTerminal myTerminalActions "one.password"
+
+data SelectedWindowTerminalActionState = SelectedWindowTerminalActionState String Int deriving (Typeable)
+instance ExtensionClass SelectedWindowTerminalActionState where
+  initialValue = SelectedWindowTerminalActionState "" 0
+
+smartGreedyViewSelectedWindowTerminalAction =
+  runSelectedWindowTerminalAction "smart.greedy.view" smartGreedyViewWindow
+
+greedyViewSelectedWindowTerminalAction =
+  runSelectedWindowTerminalAction "greedy.view" greedyViewWindow
+
+shiftSelectedWindowTerminalAction =
+  runSelectedWindowTerminalAction "shift" $ \w -> windows $ \s -> W.shiftMaster $ W.focusWindow w $ W.shiftWin (W.currentTag s) w s
+
+runSelectedWindowTerminalAction myname handler predicates = do
+    s <- gets windowset
+    let w = W.peek s
+    runCyclicTerminalAction myTerminal myname $
+                            L.map (\(header, predicate) -> selectWindowTerminalAction w header predicate .>> handler) predicates
+
+selectWindowTerminalAction w header predicate  = do
+  selectWindowTerminalActionTemplate .<. ((windowMap' predicate) >>= (\l -> return $ ((maybe "0" show w) ++ " " ++ header):(L.map (\(s, w) -> (show w) ++ " " ++ s) l)))
+windowMap' :: (WindowSet -> WindowSpace -> Bool) -> X [(String,Window)]
+windowMap' predicate = do
+    ws <- gets windowset
+    let workspaces = L.sortBy compareWorkspaceAsWorkspaceFamily $ W.workspaces ws
+    let windows = foldr (++) [] $ map (W.integrate' . W.stack) $ filter (predicate ws) workspaces
+    maxClassLength <- fmap (foldr max 0) $ mapM (fmap length . getClass') windows
+    wins <- mapM (keyValuePair maxClassLength ws) windows
+    return wins
+ where keyValuePair maxClassLength ws w = flip (,) w `fmap` (decorateName' maxClassLength ws w)
+decorateName' :: Int -> WindowSet -> Window -> X String
+decorateName' maxClassLength ws w = do
+  name <- getName' w
+  clazz <- getClass' w
+  workspace <- getWorkspace' w
+  let workspaces = W.workspaces ws
+  let focuses = L.map W.focus $ catMaybes $ L.map (W.stack) workspaces
+  let classifier = if L.elem w focuses then "* " else "  "
+  return ("[" ++ workspace ++ "] " ++ classifier ++ clazz ++ (replicate (maxClassLength - (length clazz)) ' ') ++ " : " ++ name)
+
+getName' :: Window -> X String
+getName' w = withDisplay $ \d -> do
+    -- TODO, this code is ugly and convoluted -- clean it up
+    let getIt = bracket getProp (xFree . tp_value) (copy)
+        getProp = (internAtom d "_NET_WM_NAME" False >>= getTextProperty d w)
+                      `E.catch` \(SomeException _) -> getTextProperty d w wM_NAME
+        copy prop = fromMaybe "" . listToMaybe <$> wcTextPropertyToTextList d prop
+    io $ getIt `E.catch` \(SomeException _) ->  (resName) `fmap` getClassHint d w
+getClass' :: Window -> X String
+getClass' w = withDisplay $ \d -> do
+    -- TODO, this code is ugly and convoluted -- clean it up
+    let getIt = bracket getProp (xFree . tp_value) (copy)
+        getProp = getTextProperty d w wM_CLASS
+        copy prop = fromMaybe "" . listToMaybe <$> wcTextPropertyToTextList d prop
+    io $ getIt `E.catch` \(SomeException _) ->  (resName) `fmap` getClassHint d w
+getWorkspace' :: Window -> X String
+getWorkspace' w = withWindowSet $ \s -> do
+                    case W.findTag w s >>= readWorkspaceInFamily of
+                      Just wif -> return $ (familyId wif) ++ "|" ++ (workspaceId wif)
+                      Nothing -> return ""
+
+smartGreedyViewWindow = greedyViewWindow' True
+greedyViewWindow = greedyViewWindow' False
+
+greedyViewWindow' screenAware w  = do
+  s <- gets windowset
+  case W.findTag w s of
+    Just tag -> do
+      ifX screenAware $ do
+        let fidMaybe = toFamilyIdMaybe tag
+        caseMaybeJust (L.find ((fidMaybe ==) . toFamilyIdMaybe . W.tag . W.workspace) $ W.visible s) $ viewScreen . W.screen
+      windows $ (W.focusWindow w) . (W.greedyView tag)
+    Nothing -> windows $ W.focusWindow w
+
+spawnAppSelectedTerminalAction' apps =
+    runCyclicTerminalAction myTerminal "spawn.app" [mySpawnSelectedAppTerminalAction apps, dmenuRunTerminalAction]
+
+myRunSelectedXTerminalAction = runSelectedXTerminalAction myTerminal selectActionTerminalActionTemplate
+mySpawnSelectedAppTerminalAction = spawnSelectedAppTerminalAction selectActionTerminalActionTemplate
+mySelectedXTerminalAction = selectedXTerminalAction selectActionTerminalActionTemplate
+
+runDmenuRunTerminalAction = runTerminalAction myTerminal dmenuRunTerminalAction
+
+runOpenDashboardTerminalAction = do
+  runCyclicTerminalAction myTerminal "open.dashboard" $ L.map (\script -> openDashboardTerminalAction { actionScript = script }) [
+                                  "~/.xmonad/terminal_actions/open_cluster_dashboard.sh",
+                                  "~/.xmonad/terminal_actions/open_host_dashboard.sh",
+                                  "~/.xmonad/terminal_actions/open_I.sh",
+                                  "~/.xmonad/terminal_actions/open_I_K.sh"]
+runOpenITerminalAction = do
+  runCyclicTerminalAction myTerminal "open.I" $ L.map (\script -> openDashboardTerminalAction { actionScript = script }) [
+                                  "~/.xmonad/terminal_actions/open_I.sh",
+                                  "~/.xmonad/terminal_actions/open_I_K.sh"]
